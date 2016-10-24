e55d7b982cc79b20c6d0b7c2a44f096019578519
Michael Stokley <michael@michaelstokley.com>
2016-10-23 18:15:05 -0700

    msg

--- install-python-3.5.sh
+++ install-python-3.5.sh
@@ -2,6 +2,6 @@ sudo apt-get install postgresql libpq-dev libsqlite3-dev libncursesw5-dev libssl
 wget https://www.python.org/ftp/python/3.5.2/Python-3.5.2.tar.xz
 tar xvfJ Python-3.5.2.tar.xz
 cd Python-3.5.2/
-./configure --prefix=/opt/Python3.5.2
+./configure --prefix=/opt/python3.5.2
 make
 sudo make install
